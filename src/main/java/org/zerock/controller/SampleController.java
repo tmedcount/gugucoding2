948fb94b50401cbbbf00ebf1da5e44f4382eff44
yamto <yamto@118.42.187.153>
2022-02-09 14:43:16 +0900

    설정 복원 및 @DateTimeFormat(@InitBinder 필요 없음)

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -23,11 +23,11 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
-	}
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//	}
 	
 	@RequestMapping("")
 	public void basic() {
--- src/main/java/org/zerock/domain/TodoDTO.java
+++ src/main/java/org/zerock/domain/TodoDTO.java
@@ -2,11 +2,15 @@ package org.zerock.domain;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 
 @Data
 public class TodoDTO {
 	
 	private String title;
+	
+	@DateTimeFormat(pattern = "2018/01/01")
 	private Date dueDate;
 }
