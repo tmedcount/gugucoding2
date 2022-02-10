c099d0af4df90bbfa0b91ba93cf8930003771bf2
yamto <yamto@118.42.187.153>
2022-02-10 13:20:49 +0900

    파일 업로드 연습

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,18 +1,16 @@
 package org.zerock.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -105,5 +103,18 @@ public class SampleController {
 		log.info("re2.......................");
 	}
 	
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("exUpload...............");
+	}
+	
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info(file.getOriginalFilename());
+			log.info(file.getSize());
+			log.info(file.getContentType());
+		});
+	}
 	
 }
